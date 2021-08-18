f5324ba1343377bac5fbd7771400b0df41c7995a
Artur <arturduartemoraes@gmail.com>
2021-08-18 10:31:45 -0300

    Remoção de acentos e correção do professor

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function calculoImc() {
     } else {
         const valorImc = (peso / (altura * altura)).toFixed(1)
 
-<<<<<<< HEAD
         let classificacao = '';
 
         if (valorImc < 18.5) {
@@ -29,25 +28,6 @@ function calculoImc() {
         }
 
         resultado.textContent = `Olá ${nome}! Seu IMC é  ${valorImc} e você está ${classificacao}`
-=======
-        let classificação = '';
-
-        if (valorImc < 18.5) {
-            classificação = 'abaixo do peso. :('
-        } else if (valorImc >= 18.5 && valorImc < 25) {
-            classificação = 'com o peso ideal! :)'
-        } else if (valorImc >= 25 && valorImc < 30) {
-            classificação = 'acima do peso. :('
-        } else if (valorImc >= 30 && valorImc < 35) {
-            classificação = 'com obesidade grau I. :('
-        } else if (valorImc >= 35 && valorImc < 40) {
-            classificação = 'com obesidade grau II. :('
-        } else {
-            classificação = 'com obesidade grau III. :('
-        }
-
-        resultado.textContent = `Olá ${nome}! Seu IMC é  ${valorImc} e você está ${classificação}`
->>>>>>> 976e0cac56a3771e126a61d67f8977799c3123cf
     }
 }
 
