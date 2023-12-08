cf7084187c2084dddd0b10842bdadcddad28dd64
Artur Duarte <arturduartemoraes@gmail.com>
2023-12-08 15:33:25 -0300

    ♻️ refactor: indenting code

--- professor.js
+++ professor.js
@@ -1,53 +1,52 @@
 function calculoImc(peso, altura) {
-    return (peso / (altura ** 2)).toFixed(1)
+	return (peso / altura ** 2).toFixed(1);
 }
 
 function classificar(imc) {
+	let classificacao = '';
 
-    let classificacao = '';
+	if (imc < 18.5) {
+		classificacao = 'abaixo do peso. :(';
+	} else if (imc >= 18.5 && imc < 25) {
+		classificacao = 'com o peso ideal! :)';
+	} else if (imc >= 25 && imc < 30) {
+		classificacao = 'acima do peso. :(';
+	} else if (imc >= 30 && imc < 35) {
+		classificacao = 'com obesidade grau I. :(';
+	} else if (imc >= 35 && imc < 40) {
+		classificacao = 'com obesidade grau II. :(';
+	} else {
+		classificacao = 'com obesidade grau III. :(';
+	}
 
-    if (imc < 18.5) {
-        classificacao = 'abaixo do peso. :('
-    } else if (imc >= 18.5 && imc < 25) {
-        classificacao = 'com o peso ideal! :)'
-    } else if (imc >= 25 && imc < 30) {
-        classificacao = 'acima do peso. :('
-    } else if (imc >= 30 && imc < 35) {
-        classificacao = 'com obesidade grau I. :('
-    } else if (imc >= 35 && imc < 40) {
-        classificacao = 'com obesidade grau II. :('
-    } else {
-        classificacao = 'com obesidade grau III. :('
-    }
-
-    return classificacao
+	return classificacao;
 }
 
 function camposValidos() {
-    return document.querySelector('form').reportValidity()
+	return document.querySelector('form').reportValidity();
 }
 
 function exibirResultado() {
-    const nome = document.getElementById("nome").value
-    const altura = parseFloat(document.getElementById("altura").value)
-    const peso = parseFloat(document.getElementById("peso").value)
-    const resultado = document.getElementById("resultado")
+	const nome = document.getElementById('nome').value;
+	const altura = parseFloat(document.getElementById('altura').value);
+	const peso = parseFloat(document.getElementById('peso').value);
+	const resultado = document.getElementById('resultado');
 
-    if (camposValidos) {
-        const imc = calculoImc(peso, altura)
-        const classificacao = classificar(imc)
+	if (camposValidos) {
+		const imc = calculoImc(peso, altura);
+		const classificacao = classificar(imc);
 
-        resultado.textContent = `Olá ${nome}! Seu IMC é  ${imc} e você está ${classificacao}`
-    } else {
-        resultado.textContent = 'Preencha todos os campos!'
-    }
+		resultado.textContent = `Olá ${nome}! Seu IMC é  ${imc} e você está ${classificacao}`;
+	} else {
+		resultado.textContent = 'Preencha todos os campos!';
+	}
 }
 
 function capturarEnter(event) {
-    if (event.key == "Enter") {
-        exibirResultado()
-    }
+	if (event.key == 'Enter') {
+		exibirResultado();
+	}
 }
 
-document.getElementById("botao").addEventListener('click', exibirResultado)
-document.querySelector('form').addEventListener('keypress', capturarEnter)
+document.getElementById('botao').addEventListener('click', exibirResultado);
+document.querySelector('form').addEventListener('keypress', capturarEnter);
--- script.js
+++ script.js
@@ -1,48 +1,52 @@
-const botao = document.getElementById("botao")
+'use strict';
+
+const botao = document.getElementById('botao');
 
 function calculoImc() {
-    const nome = document.getElementById("nome").value
-    const altura = document.getElementById("altura").value
-    const peso = document.getElementById("peso").value
-    const resultado = document.getElementById("resultado")
+	const nome = document.getElementById('nome').value;
+	const altura = document.getElementById('altura').value;
+	const peso = document.getElementById('peso').value;
+	const resultado = document.getElementById('resultado');
 
-    if (nome == '' || altura == '' || peso == '') {
-        resultado.textContent = 'Preencha todos os campos!'
-    } else {
-        const valorImc = (peso / (altura * altura)).toFixed(1)
+	if (nome == '' || altura == '' || peso == '') {
+		resultado.textContent = 'Preencha todos os campos!';
+	} else {
+		const valorImc = (peso / (altura * altura)).toFixed(1);
 
-        let classificacao = '';
-        let frase = '';
+		let classificacao = '';
+		let frase = '';
 
-        if (valorImc < 18.5) {
-            classificacao = 'abaixo do peso. <span class="red">:(</span>'
-            frase = 'Cuidado'
-        } else if (valorImc >= 18.5 && valorImc < 25) {
-            classificacao = 'com o peso ideal! <span class="green">:)</span>'
-            frase = 'Parabéns'
-        } else if (valorImc >= 25 && valorImc < 30) {
-            classificacao = 'acima do peso. <span class="red">:(</span>'
-            frase = 'Cuidado'
-        } else if (valorImc >= 30 && valorImc < 35) {
-            classificacao = 'com obesidade grau I. <span class="red">:(</span>'
-            frase = 'Cuidado'
-        } else if (valorImc >= 35 && valorImc < 40) {
-            classificacao = 'com obesidade grau II. <span class="red">:(</span>'
-            frase = 'Cuidado'
-        } else {
-            classificacao = 'com obesidade grau III. <span class="red">:(</span>'
-            frase = 'Cuidado'
-        }
+		if (valorImc < 18.5) {
+			classificacao = 'abaixo do peso. <span class="red">:(</span>';
+			frase = 'Cuidado';
+		} else if (valorImc >= 18.5 && valorImc < 25) {
+			classificacao = 'com o peso ideal! <span class="green">:)</span>';
+			frase = 'Parabéns';
+		} else if (valorImc >= 25 && valorImc < 30) {
+			classificacao = 'acima do peso. <span class="red">:(</span>';
+			frase = 'Cuidado';
+		} else if (valorImc >= 30 && valorImc < 35) {
+			classificacao = 'com obesidade grau I. <span class="red">:(</span>';
+			frase = 'Cuidado';
+		} else if (valorImc >= 35 && valorImc < 40) {
+			classificacao =
+				'com obesidade grau II. <span class="red">:(</span>';
+			frase = 'Cuidado';
+		} else {
+			classificacao =
+				'com obesidade grau III. <span class="red">:(</span>';
+			frase = 'Cuidado';
+		}
 
-        resultado.innerHTML = `Olá ${nome}! Seu IMC é  ${valorImc} e você está ${classificacao} `
-    }
+		resultado.innerHTML = `Olá ${nome}! Seu IMC é  ${valorImc} e você está ${classificacao} `;
+	}
 }
 
 function capturarEnter(event) {
-    if (event.key == "Enter") {
-        calculoImc()
-    }
+	if (event.key == 'Enter') {
+		calculoImc();
+	}
 }
 
-botao.addEventListener('click', calculoImc)
-document.querySelector('form').addEventListener('keypress', capturarEnter)
+botao.addEventListener('click', calculoImc);
+document.querySelector('form').addEventListener('keypress', capturarEnter);
